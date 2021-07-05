495b7c1bd40473020a751a7f508d1a95648be376
Rafael Angonese <rafael.angonese09@gmail.com>
2021-07-04 22:00:59 -0300

    remove coments

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import { useRouter } from 'next/router';
 
 import Navbar from '../Navbar';
 
@@ -7,15 +6,7 @@ const Layout: FC = ({ children }) => {
     return (
         <>
             <Navbar />
-            <main className="bg-gray-50 min-h-screen">
-                asdfas sfsafsfsaf das fsadf
-                <br />
-                <br />
-                <br />
-                <br />
-                asdfas sfsafsfsaf das fsadf
-                {children}
-            </main>
+            <main className="bg-gray-50 min-h-screen">{children}</main>
         </>
     );
 };
