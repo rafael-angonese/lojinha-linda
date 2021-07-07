dadd261bb46dc7ed3b5cdf99a197d727af0f7703
Rafael Angonese <rafael.angonese09@gmail.com>
2021-07-07 19:50:51 -0300

    search page

---
+++ src/pages/search.tsx
@@ -0,0 +1,23 @@
+import ProductCard from '../components/ProductCard';
+
+export default function Search() {
+    return (
+        <>
+            <section className="text-gray-400 bg-gray-900 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
