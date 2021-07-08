5e9ffc1b68eacaded4fd46a6734142bd4cc84e69
Rafael Angonese <rafael.angonese09@gmail.com>
2021-07-07 22:15:03 -0300

    include SidebarContextProvider on Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,10 +2,14 @@ import React, { FC } from 'react';
 
 import Navbar from '../Navbar';
 
+import { SideBarContextProvider } from '../../contexts/SideBarContext';
+
 const Layout: FC = ({ children }) => {
     return (
         <>
-            <Navbar />
+            <SideBarContextProvider>
+                <Navbar />
+            </SideBarContextProvider>
             <main className="bg-gray-900 min-h-screen">{children}</main>
         </>
     );
