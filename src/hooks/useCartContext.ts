92f72ad58606de2f3dae1d57592ee708bcd8cdcb
Rafael Angonese <rafael.angonese09@gmail.com>
2021-07-15 16:21:16 -0300

    create cart hook

---
+++ src/hooks/useCartContext.ts
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
+
+export function useCartContext() {
+    const context = useContext(CartContext);
+
+    return context;
+}
